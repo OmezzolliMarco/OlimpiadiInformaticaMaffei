edc3f4830f2974f048b70bc54879793427c8d9a4
MarcusOme <omezzollimarco@gmail.com>
2025-11-06 14:49:51 +0100

    Inizio esercizio

---
+++ Greedy/nimbus3000.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# NOTA: si raccomanda di usare questo template anche se non lo si capisce completamente.
+
+import sys
+
+# decommenta le due righe seguenti se vuoi leggere/scrivere da file
+sys.stdin = open('Greedy\\input.txt')
+#sys.stdout = open('output.txt', 'w')
+
+T = int(input().strip())
+lista_coppie = []
+for test in range(1, T+1):
+    N, M = map(int, input().strip().split())
+
+    lista_coppie.append((N, M))
+
+    print(lista_coppie)
+
+    x = 0
+
+    # INSERISCI IL TUO CODICE QUI
+
+
+    print("Case #%d: " % test, end='')
+    print(x)
+
+sys.stdout.close()
